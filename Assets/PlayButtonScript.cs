131d586960674b045269946a239fdc91a009bb62
Aaron He <mucube0@gmail.com>
2023-06-20 21:21:22 -0600

    Add five pin mode

--- Assets/PlayButtonScript.cs
+++ Assets/PlayButtonScript.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayButtonScript : MonoBehaviour
 {
+    public TMP_Dropdown chooseMode;
     public void Play() {
-        SceneManager.LoadScene("TenPin");
+        int mode = chooseMode.value;
+        
+        if (mode == 0) {
+            SceneManager.LoadScene("TenPin");
+        }
+        if (mode == 1) {
+            SceneManager.LoadScene("FivePin");
+        }
     }
 }
