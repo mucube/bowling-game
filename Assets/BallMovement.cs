a9f1b1d1fbcaee24faff18034a31d5d3965cedc7
Aaron <mucube0@gmail.com>
2023-06-26 16:53:13 -0600

    Add wasd support

--- Assets/BallMovement.cs
+++ Assets/BallMovement.cs
@@ -15,7 +15,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("left") || Input.GetKey("right") || Input.GetKey("up") || Input.GetKey("down")) {
+        if (Input.GetKey("left") || Input.GetKey("right") || Input.GetKey("up") || Input.GetKey("down") || Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d")) {
             Vector3 movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             body.AddForce(movementDirection * Time.deltaTime * speed);
         }
